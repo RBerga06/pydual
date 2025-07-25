7b3d2fa2748eb0e1b89f5289f56380c405a28e79
rberga06 <78449715+RBerga06@users.noreply.github.com>
2025-07-25 17:20:25 +0200

    fix(build): switch to uv build backend

--- src/pydual/core/__init__.py
+++ src/pydual/core/__init__.py
@@ -1,17 +1,4 @@
 """Dual numbers for Python."""
-from dataclasses import dataclass
+from .array import dual
 
-
-@dataclass(frozen=True)
-class dual[T = float]:
-    """Minimal implementation of a dual number."""
-
-    real: T
-    dual: T
-
-    def __add__(self, rhs: T, /) -> "dual[T]":
-        return self
-
-
-def foo(x: dual[float]):
-    pass
+__all__ = ["dual"]
